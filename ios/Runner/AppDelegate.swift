87f3780ccfcb144e2030c8940b1226dab05a99fd
luisgc020300-beep <luisgc020300@gmail.com>
2026-02-21 00:52:46 +0100

    solucion4

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,15 @@
-import Flutter
 import UIKit
+import Flutter
+import FirebaseCore // Añadimos esto manualmente
+import cloud_firestore // Añadimos esto manualmente
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure() // Configuramos Firebase antes que nada
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
